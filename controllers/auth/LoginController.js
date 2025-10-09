6a2b9ac8f56d60a9c59f53c8b7500732f9f28e17
ryan.lintag@excentone.com <ryan.lintag@excentone.com>
2025-10-09 11:17:59 +0800

    Fixed the error

--- controllers/auth/LoginController.js
+++ controllers/auth/LoginController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../../model/User.js';
+import User from '../../model/user.js';
 
 
 export const login = async (req, res) => {
